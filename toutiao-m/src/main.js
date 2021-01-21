af5e760e9e3f3d5f064b5455e56e237f5c0b2436
bendanykykyk <446679223@qq.com>
2021-01-21 20:18:36 +0800

    修改初始目录结构

--- toutiao-m/src/main.js
+++ toutiao-m/src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+// 加载全局样式
+import "style/index.less";
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
-
+// 把 App 根组件渲染/替换到 #app节点
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
---
+++ toutiao-m/vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        //取别名，方便寻资源
+        assets: "@/assets",
+        style: "@/style",
+        common: "@/common",
+        components: "@/components",
+        network: "@/network",
+        views: "@/views"
+      }
+    }
+  }
+};
