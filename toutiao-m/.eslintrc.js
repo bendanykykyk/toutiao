b0f73c08684140ba60935f5f42842934724b3792
bendanykykyk <446679223@qq.com>
2021-01-21 16:46:55 +0800

    init

---
+++ toutiao-m/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential", "@vue/standard"],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    quotes: [1, "double"], // 1 违反错误警告
+    semi: [2, "always"] //语句强制分号结尾
+  }
+};
